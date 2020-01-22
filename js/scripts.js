6c0f61bc1f1488db9b5b30e3d33e46d294c7668e
Sebastian Rico Brenner <sebastian_rico.brenner@student.reutlingen-university.de>
2020-01-22 08:15:50 +0100

    make screen switch work change iframe to "smaller" iframe without menu bar add original svgs add new pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,75 +1,35 @@
-screen = {
-  aInternal: "menu",
-  aListener: function(val) {},
-  set a(val) {
-    this.aInternal = val;
-    this.aListener(val);
-  },
-  get a() {
-    return this.aInternal;
-  },
-  registerListener: function(listener) {
-    this.aListener = listener;
-  }
-};
-
-screen.registerListener(function(val) {
-  showScreen(val);
-});
-
-window.addEventListener(
-  "message",
-  function(event) {
-    if (event.origin === "http://localhost/") {
-      alert("Received message: " + event.data.message);
-    } else {
-      alert("Origin not allowed!");
-    }
-  },
-  false
-);
-
-function loadScreen() {
-  document.getElementById("screen").innerHTML =
-    '<object type="text/html" data="welcome.html"></object>';
-}
-
 async function loadWelcomeScreen() {
-  document.getElementById("screen").innerHTML =
-    '<iframe id="screenFrame" class="screenFrame" src="welcome.html"></iframe>';
-  $("#screenFrame").hide();
-  $("#screenFrame").fadeIn("slow");
+  showScreen("welcome");
+  $("#emptyScreenFrame").hide();
+  $("#emptyScreenFrame").fadeIn("slow");
   await sleep(4000);
-  clearScreen();
+  showScreen("empty");
   await sleep(1000);
-  showMenu();
-}
-
-function clearScreen() {
-  document.getElementById("screen").innerHTML = "";
+  showScreen("menu");
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function showMenu() {
-  document.getElementById("screen").innerHTML =
-    '<iframe id="screenFrame" name="screenFrame" class="screenFrame" src="menu.html"></iframe>';
-}
-
-function showFehler() {
-  screen = "fehler";
-}
-
 function showScreen(screenName) {
-  alert(window.parent);
-  /*if (screenName == "empty") {
-    parent.getElementById("screen").innerHTML = "";
+  if (screenName == "welcome") {
+    $("#emptyScreenFrame").attr("src", screenName + ".html");
+    $("#emptyScreen").show();
+  } else if (screenName == "empty") {
+    $("#emptyScreen").show();
+    $("#emptyScreenFrame").attr("src", "");
   } else {
-    parent.getElementById("window.screen").innerHTML =
-      '<iframe id="screenFrame" class="screenFrame" src="' +
-      screenName +
-      '.html"></iframe>';
-  }*/
+    $("#screenFrame").attr("src", screenName + ".html");
+    $("#emptyScreen").hide();
+  }
+}
+
+function showScreenFrame(screenName) {
+  location.href = screenName + ".html";
+}
+
+function debug() {
+  console.log("click");
+  location.href = "fehler.html";
 }
